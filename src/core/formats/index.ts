3a8ec56e87e782abcf00f103bfb72993714a2cdb
VishalPainjane <painjanevishal2204@gmail.com>
2025-12-18 15:57:51 +0530

    chore(add): add src/core/formats/index.ts

---
+++ src/core/formats/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Model format parsers
+ * Handles conversion of various model file formats to NN3D
+ */
+
+export * from './types';
+export * from './onnx-parser';
+export * from './safetensors-parser';
+export * from './pytorch-parser';
+export * from './keras-parser';
+export * from './format-detector';
